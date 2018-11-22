112503b57d76ef7dd3e5309f1e43e128014d7b2d
Ikaro Alef <ikaroalef@gmail.com>
2018-11-22 14:23:18 -0300

    verifica se o servidor esta rodando na nossa Main

--- src/ikaro/Main.java
+++ src/ikaro/Main.java
@@ -1,11 +1,33 @@
 package ikaro;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.UnknownHostException;
 
 public class Main {
-	public static void main(String[] args) throws UnknownHostException {
+	public static void main(String[] args) throws IOException {
 		CommandTeam teamA = new CommandTeam();
-		teamA.launchTeamAndServer();
-		//teamA.launchTeam();
+		//se o processo do ervidor está rodando, lança só o time
+		//se não, lança o time e o servidor
+		if(isProcessRunning()) 
+			teamA.launchTeam();
+		else teamA.launchTeamAndServer();
+		
+	}
+	
+	//verificar se o servidor já está rodando (pra lançar apenas o time ou o time e o servidor)
+	private static boolean isProcessRunning() throws IOException{
+		String line;
+		String pidInfo = "";
+		Process p = Runtime.getRuntime().exec(System.getenv("windir")+"\\system32\\tasklist.exe");
+		BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		while((line = input.readLine())!= null){
+			pidInfo+=line;
+		}
+		input.close();
+		if(pidInfo.contains("rcssserver.exe"))
+			return true;
+		return false;
 	}
 }
